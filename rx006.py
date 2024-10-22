87507b2ecbb5f9af7e5e041dceadda6378bcd489
Sergio Roberto de Jesus <114026661+sergiorobertodejesus@users.noreply.github.com>
2024-10-22 06:45:15 -0300

    Create rx006.py

---
+++ rx006.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um numero:'))
+raiz = numero **(1/2)
+print(f'O dobro de {numero} vale {numero*2}')
+print(f'o triplo de {numero} vale {numero*3}')
+print(f'a raiz quadrada de {numero} é {raiz:.2f}')
