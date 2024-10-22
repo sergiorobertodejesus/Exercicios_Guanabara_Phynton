bb72b5dc3316767e63e452949af896145dcd6ff1
Sergio Roberto de Jesus <114026661+sergiorobertodejesus@users.noreply.github.com>
2024-10-22 07:44:06 -0300

    medidas

---
+++ ex007.py
@@ -0,0 +1,4 @@
+m1 = float(input('qual a primeira nota do aluno: '))
+m2 = float(input('qual a segunda nota do aluno: '))
+media = (m1 + m2) / 2
+print(f'a primeira nota {m1:.2f} a segunda nota {m2:.2f} da a media de {media:.2f}')
---
+++ ex008.py
@@ -0,0 +1,11 @@
+distancia = float(input('Uma distancia em metros: '))
+print(f'''
+  a medida de {distancia} corresponde a \n
+  {distancia/1000}km \n
+  {distancia/100}hm \n
+  {distancia/10}dam \n
+  {distancia} \n
+  {distancia*100}cm \n
+  {distancia*1000}mm
+  
+      ''')
