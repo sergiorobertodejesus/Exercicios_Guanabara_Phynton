db040691fcb0900542cc941e2bdf346cd3439150
Sergio Roberto de Jesus <114026661+sergiorobertodejesus@users.noreply.github.com>
2024-10-22 19:20:46 -0300

    atualizaçao

---
+++ ex009.py
@@ -0,0 +1,13 @@
+tabuada = int(input(' Digite um numero para ver sua tabuada : '))
+print(f'''
+      {tabuada} x 1 =  {tabuada*1}\n
+      {tabuada} x 2 =  {tabuada*2}\n
+      {tabuada} x 3 =  {tabuada*3}\n
+      {tabuada} x 4 =  {tabuada*4}\n
+      {tabuada} x 5 =  {tabuada*5}\n
+      {tabuada} x 6 =  {tabuada*6}\n
+      {tabuada} x 7 =  {tabuada*7}\n
+      {tabuada} x 8 =  {tabuada*8}\n
+      {tabuada} x 9 =  {tabuada*9}\n
+      {tabuada} x 10 = {tabuada*10} 
+      ''')
---
+++ ex010.py
@@ -0,0 +1,2 @@
+dinheiro = float(input(' quanto dinheiro vc tem na carteira:R$ '))
+print(f'com R${dinheiro:.2f} voçe consegue pode comprar US${dinheiro/5.69:.2f}')
