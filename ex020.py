c85e35fe368db38a37139c6fa9cdb17b07f45846
Sergio Roberto de Jesus <h.desk4024@gmail.com>
2024-11-22 19:05:19 -0300

    atualizaçao

--- ex020.py
+++ ex020.py
@@ -1,10 +1,8 @@
-# importar a biblioteca pygame
-from pygame import mixer
-
-# uma forma de implementar o código
-mixer.init()
-
-# substitua o nome do arquivo "musica.mp3" pelo seu arquivo mp3
-mixer.music.load('KillBill.mp3')
-mixer.music.play()
-x = input('Digite algo para parar...')
+import random
+a1 = str(input("Primeiro Aluno: "))
+a2 = str(input("Segundo Aluno: "))
+a3 = str(input("Terceiro Aluno: "))
+a4 = str(input("Quarto Aluno: "))
+alunos = [a1,a2,a3,a4]
+random.shuffle(alunos)
+print(f"A ORDEM APRESENTADA SERA {alunos}")
