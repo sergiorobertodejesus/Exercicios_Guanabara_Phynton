f692dfbd936688bc1546adffd2e8fc87e1c109be
Sergio Roberto de Jesus <h.desk4024@gmail.com>
2024-11-26 15:53:50 -0300

    numeros maiores

---
+++ ex038.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro valor:"))
+num2 = int(input(("Digite o segundo valor: ")))
+if num1 > num2:
+    print("o primeiro é maior")
+elif num2 > num1:
+    print("o segundo é maior")
+else:
+    num1 == num2
+    print("são iguais")
