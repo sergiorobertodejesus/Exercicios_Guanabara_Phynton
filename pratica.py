e78f8d0ca98f72632190a0145ea056a70ba3dc62
Sergio Roberto de Jesus <114026661+sergiorobertodejesus@users.noreply.github.com>
2025-01-08 10:59:32 -0300

    teste

--- pratica.py
+++ pratica.py
@@ -1,15 +1,1 @@
-print( )
-print('-'*30)
-print('Sequencia de Fibonacci')
-print('-'*30)
-n = int(input('Quantos termo vc quer ver: '))
-t1 = 0
-t2 = 1
-print(f'{t1} -> {t2}',end='')
-cont = 3
-while cont <= n:
-    t3 = t1 + t2
-    cont += 1
-    t1 = t2
-    t2 = t3
-    print(f' -> {t3}',end='')
+print('hello my eggs')
