0edd64d1e774c4288d34a772c75f1955a732a03a
Sergio Roberto de Jesus <114026661+sergiorobertodejesus@users.noreply.github.com>
2025-01-12 08:44:31 -0300

    pra

--- pratica.py
+++ pratica.py
@@ -1,1 +1,19 @@
-print('hello my eggs')
+soma = quant = media = maior = menor = 0
+while True:
+    num = int(input("digite um numero : "))
+    soma += num 
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input("Quer continuar [S N]: ")).upper().strip()[0]
+    if resp == "N":
+        break
+media = soma / quant
+print(f'foram digitados {quant} numeros e a media é {media} ')
+print(f" e o maior numero foi {maior} e o menor numero foi {menor}")
+    
---
+++ pratica2.py
@@ -0,0 +1,8 @@
+num = 0
+while True:
+    num += 1
+    print(f'contagem:{num}')
+    if num == 10:
+        break
+print('fim')    
+        
